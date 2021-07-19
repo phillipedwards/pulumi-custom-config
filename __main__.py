b9e4d9359ff86c1dff620c5753374bc07a935b93
Phillip Edwards <phillip@pulumi.com>
2021-07-19 14:05:56 -0700

    clean up dynamic providers and logging

--- __main__.py
+++ __main__.py
@@ -3,33 +3,11 @@
 """Work around to accomplish https://github.com/pulumi/pulumi/issues/7048#issuecomment-868100604"""
 
 import pulumi
-from pulumi.resource import ResourceOptions, ResourceTransformationArgs, ResourceTransformationResult
+from pulumi.resource import ResourceOptions
 from pulumi_aws.ec2 import vpc
 import custom_config
 
-from pulumi_aws import s3
-from pulumi_aws import provider
 from pulumi_aws import ec2
-
-def stack_transformation(args: ResourceTransformationArgs):
-    pulumi.log.debug(f'Transforming for {args.type_}')
-    return ResourceTransformationResult(
-        props=args.props,
-        opts=ResourceOptions.merge(args.opts, ResourceOptions(
-            provider=aws_provider
-        ))
-    )
-
-def define_aws_provider():
-    """
-    If one would like to override the aws region specified in the pulumi config, we can use the following
-    This will define an aws provider to align with our change in region
-    """
-    aws_config = custom_config.CustomConfig('aws')
-
-    print('Configuring AWS region: ', aws_config.get_string('region'))
-
-    return provider.Provider("aws_custom_provider", region=aws_config.get_string("region"))
 
 """
 This is an example of a intermediate layer, on top of pulumi's config, to show how one could implement heirarchical configs and directories to help organize multi-tenant configurations in a pulumi project
@@ -41,32 +19,24 @@ Below values will be retrieved by keys from 3 different locations:
 Values will be retrieved from config files in the same order above, meaning the stack specific config will be checked first and the pulumi config last.
 """
 
-aws_provider = define_aws_provider()
-
-"""
-Register 'stack_transformation' to force all resources to use the explicit user-defined aws-provider
-This will ensure our resources use the chosen region
-"""
-pulumi.runtime.register_stack_transformation(stack_transformation)
-
 """
 Example # 1 - Retrieve value of our EC2 instance type by key, from the customer stack config
 """
 ec2_config = custom_config.CustomConfig('ec2')
-print('stack config::instance-size: ', ec2_config.get_string('instance-type'))
+print('Retrieving value from /customer_config/prod/customer123.yaml::instance-size: ', ec2_config.get_string('instance-type'))
 
 """
 Example # 2 - Retrieve value of ECS cpu and memory by key, from our default config
 """
 ecs_config = custom_config.CustomConfig('ecs')
-print('default config::ecs cpu', ecs_config.get_string('cpu'))
-print('default config::ecs memory', ecs_config.get_string('memory'))
+print('Retrieving default value from /customer_config/default.yaml::ecs cpu', ecs_config.get_string('cpu'))
+print('Retrieving default value from /customer_config/default.yaml::ecs memory', ecs_config.get_string('memory'))
 
 """
 Example #3 - Retrieve value of VPC ID by key, from our pulumi config
 """
 vpc_config = custom_config.CustomConfig("vpc")
-print('pulumi_config::vpcid', vpc_config.get_string('vpcid'))
+print('Retrieving value from /pulumi_config stack configuration::vpcid', vpc_config.get_string('vpcid'))
 
 sg = ec2.SecurityGroup('sec-grp-for-ec2', 
     description='security group',
@@ -82,8 +52,7 @@ Thus, we need to ensure we are explicitly passing the provider using ResourceOpt
 ami = ec2.get_ami(
     owners=['amazon'],
     most_recent=True,
-    filters=[{"name":"name","values":["amzn-ami-hvm-*"]}],
-    opts=ResourceOptions(provider=aws_provider)
+    filters=[{"name":"name","values":["amzn-ami-hvm-*"]}]
 )
 
 instance = ec2.Instance('instance',
